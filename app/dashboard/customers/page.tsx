21fa4ee3dcfeae4354ce05251246fcf4806702c7
Rey Arqueza <rarqueza@jellybelly.com>
2026-01-28 16:15:13 -0800

    ch 15 - metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,12 @@
-
 async function delay(ms: number) {
   return new Promise((res) => setTimeout(res, ms));
 }
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
 
 export default async function Page() {
   // await delay(1500);
